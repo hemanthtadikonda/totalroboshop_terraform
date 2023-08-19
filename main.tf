e227dffc1b85c0e578ec8f550a96db7ed1a810a5
hemanth <hemanthtadikonda29@gmail.com>
2023-08-19 19:08:03 +0530

    vpc terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
-module "subnets" {
+module "vpc" {
+
   source = "git::https://github.com/hemanthtadikonda/vpc_terraform_module.git"
-  for_each = var.subnets
-  vpc = var.vpc
-  subnets = var.subnets
+
+  for_each = var.vpc
+  cidr = each.value["cidr"]
+  subnets = each.value["subnets"]
+
 
 }
 
