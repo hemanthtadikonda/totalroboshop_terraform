361b8dcde03d62397af16a46737a39b2f4549485
hemanth <hemanthtadikonda29@gmail.com>
2023-08-22 18:36:02 +0530

    app load balencer set up

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   # created vpc id
-  vpc_id =  lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  vpc_id =  lookup(lookup(module.vpc, "main", null), "aws_vpc_id", null)
   app_subnets = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id]
 
 }
