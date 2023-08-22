9fc3f7985163b13d49878594c4dc6ce8d2989185
hemanth <hemanthtadikonda29@gmail.com>
2023-08-22 15:21:00 +0530

    app load balencer set up

--- vars.tf
+++ vars.tf
@@ -5,8 +5,3 @@ variable "default_route_table_id" {}
 variable "tags" {}
 variable "env" {}
 
-variable "alb" {}
-variable "internal" {}
-variable "lb_type" {}
-variable "sg_group" {}
-variable "sg_port" {}
