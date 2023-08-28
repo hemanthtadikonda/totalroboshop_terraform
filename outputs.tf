58fdf37f3dc79119bae1ed355bd4540a56114192
hemanth <hemanthtadikonda29@gmail.com>
2023-08-28 11:39:24 +0530

    tf - module - app

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "alb" {
-  value = lookup(lookup(lookup(module.alb, "private" ,null), "listner" , null) , "arn" , null)
+  value = lookup(lookup(lookup(module.alb, "private" ,null), "listener" , null) , "arn" , null)
 }
 output "vpc" {
   value = module.vpc
