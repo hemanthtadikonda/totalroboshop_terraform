50da6cce679fab29904afd6668a1f64e0d47fd62
hemanth <hemanthtadikonda29@gmail.com>
2023-10-18 18:08:50 +0530

    scripted pipeline

--- locals.tf
+++ locals.tf
@@ -6,9 +6,9 @@ locals {
 
   app_subnets_cidr_block = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.cidr_block]
 
-  private_alb_dns = lookup(lookup(lookup(module.alb, "private" , null), "alb" , null) , "dns_name" ,null)
-  private_lb_arn  = lookup(lookup(lookup(module.alb, "private" ,null), "listener" , null) , "arn" , null)
+  #private_alb_dns = lookup(lookup(lookup(module.alb, "private" , null), "alb" , null) , "dns_name" ,null)
+  #private_lb_arn  = lookup(lookup(lookup(module.alb, "private" ,null), "listener" , null) , "arn" , null)
 
-  public_alb_dns = lookup(lookup(lookup(module.alb, "public" , null), "alb" , null) , "dns_name" ,null)
-  public_lb_arn  = lookup(lookup(lookup(module.alb, "public" ,null), "listener" , null) , "arn" , null)
+  #public_alb_dns = lookup(lookup(lookup(module.alb, "public" , null), "alb" , null) , "dns_name" ,null)
+  #public_lb_arn  = lookup(lookup(lookup(module.alb, "public" ,null), "listener" , null) , "arn" , null)
 }
